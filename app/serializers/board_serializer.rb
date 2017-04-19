b313bfbb4cb1e7e43e4759259f678155d6ac412d
mwei2509 <m2dl89@gmail.com>
2017-04-19 13:00:42 -0400

    created at and updated boards

--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -1,6 +1,7 @@
 class BoardSerializer < ActiveModel::Serializer
-  attributes :id, :title
+  attributes :id, :title, :created_at, :updated_at
 
   has_many :accounts, through: :teams
   has_many :elements
+
 end
