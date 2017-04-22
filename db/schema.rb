5c90c494d7800854a8c2414e5d1accdc980bd1ce
mwei2509 <m2dl89@gmail.com>
2017-04-21 21:39:21 -0400

    add url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421213956) do
+ActiveRecord::Schema.define(version: 20170422013628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170421213956) do
     t.datetime "updated_at"
     t.string   "currentcolor"
     t.boolean  "public",       default: false
+    t.string   "url"
   end
 
   create_table "elements", force: :cascade do |t|
