3c08b3aa4c2ec9dfd3bdbcfca90796851d2fdf7c
jef4490 <jeffrey.hechler@gmail.com>
2017-04-24 09:04:19 -0400

    registrations controller testing

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -2,16 +2,23 @@ require "rails_helper"
 
 RSpec.describe RegistrationsController, :type => :controller do
 
-  # describe "POST create" do
-  #   before :each do
-  #    @account = Account.new(username: "holtford", email: "theoneandonly@holt.com", password: "mvp")
-  #  end
-  #
-  #   it "Returns an error if not passed necessary params" do
-  #     expect {
-  #       post :create, @account
-  #     }.to change(Account, :count).by(1)
-  #   end
-  # end
-  #
+  describe "POST create" do
+    before :each do
+     @account = Account.new(username: "holtford", email: "theoneandonly@holt.com", password: "mvp")
+   end
+
+    it "makes a new account and returns a token" do
+      expect {
+        post :create, params: {account: {username: @account.username, email: @account.email, password: @account.password}}
+      }.to change(Account, :count).by(1)
+      expect(response.body).to include("jwt")
+    end
+
+    it "has a 401 status code if the account is invalid" do
+      post :create, params: {account: {username: @account.username, password: @account.password}}
+      expect(response.status).to eq(401)
+    end
+
+  end
+
 end
